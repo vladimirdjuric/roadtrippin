bbaac5c7f40b57209157a28f9e7d4b4ff99acb22
vladimirdjuric <40430889+vladimirdjuric@users.noreply.github.com>
2018-06-23 18:41:04 +0200

    added dropdown

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
+$(document).ready(function(){
 
+// display social media dropdown
+let active = true;
+$('#follow').on('click', function(){
+	active = !active;
+	if(active === false) {
+		$('#dropdown').css('display', 'block');
+	}else {
+		$('#dropdown').css('display', 'none');
+	}
+});
+
+
+
+
+})
